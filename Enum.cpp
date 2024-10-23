4c90095790e65d67fde75dbe8bc33d35ff11b319
Vance <huogxu@gmail.com>
2024-10-23 13:29:48 +0800

    枚举 Enum

--- Enum.cpp
+++ Enum.cpp
@@ -10,7 +10,16 @@ enum Color {
 
 };
 
-enum class Week { MON, TUE, WED };
+// BLUE 已经在 Color 中定义，重复定义会报错
+// enum Feeling { EXCITED, BLUE };
+
+// 使用 namespace 解决作用域不受限的问题，更“有效”的办法是用一个类或结构体来限定其作用域
+namespace Feeling {
+enum Type { EXCITED, BLUE };
+}
+
+// C++11 的枚举类
+enum class Week { MON, TUE, WED, THU, FRI, SAT, SUN, BLUE };
 
 // 自定义输出流操作符
 std::ostream &operator<<(std::ostream &os, const Week &day) {
@@ -25,7 +34,7 @@ std::ostream &operator<<(std::ostream &os, const Week &day) {
             os << "Wednesday";
             break;
         default:
-            os << "Unknown day";
+            os << "Other day";
     }
     return os;
 }
@@ -37,7 +46,10 @@ int main(int argc, char const *argv[]) {
     cout << myColor << endl;
 
     // C++ 标准库没有为 enum class 定义默认的输出流操作符（operator<<）
-    cout << Week::MON << endl;
+    cout << "MON: " << Week::MON << " SUN: " << Week::SUN << endl;
+
+    Feeling::Type feeling = Feeling::BLUE;
+    cout << "Feeling::BLUE=" << feeling << endl;
 
     return 0;
 }
