e99a73aeb057594c15c176310f2d2126d2b64164
VanceKing <huogxu@gmail.com>
2025-04-09 23:29:52 +0800

    常量指针

--- const/Const2.cpp
+++ const/Const2.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+/*
+
+常量指针:
+定义: 指针指向的值是常量，不能通过该指针修改值，但指针本身可以指向其他地址。
+语法: const T* ptr 或 T const* ptr
+特点:
+1. 指针指向的值是只读的，不能通过指针修改。
+2. 指针本身可以重新赋值，指向其他变量。
+
+*/
+
 int main(int argc, char const* argv[]) {
     const int* number;
     // 输出指针的地址：0x7fff0937a258
@@ -9,8 +20,14 @@ int main(int argc, char const* argv[]) {
 
     // 定义变量 value
     int value = 6;
+    int value2 = 7;
     // 取变量 value 的地址并赋值给指针 number
     number = &value;
+    cout << "number: " << *number << endl;
+    // *number = 7;  // error, 不能赋值
+
+    // 取变量 value2 的地址并赋值给指针 number
+    number = &value2;
     cout << "number: " << *number << endl;
 
     int* number2 = &value;
