705a7c2a5534530cb461e896343dc22f6196e50e
Vance <huogxu@gmail.com>
2025-04-10 16:46:01 +0800

    常量指针和指针常量

--- const/ConstPointer.cpp
+++ const/ConstPointer.cpp
@@ -2,15 +2,39 @@
 
 using namespace std;
 
+/*
+
+常量指针:
+定义: 指针指向的值是常量，不能通过该指针修改值，但指针本身可以指向其他地址。
+语法: const T* ptr 或 T const* ptr
+特点:
+1. 指针指向的值是只读的，不能通过指针修改。
+2. 指针本身可以重新赋值，指向其他变量。
+
+*/
 void test1() {
+    int value = 6;
+    int value2 = 7;
     const int number = 10;  // 常量
-    const int* number2;     // 指向常量的指针
+    const int* numberPtr;   // 指向常量的指针
+
+    // 取变量 number 的地址并赋值给指针 numberPtr
+    numberPtr = &number;
+    // 因为 numberPtr 不是 const，所以可以重新赋值。
+    numberPtr = &value;
+
     /*
-    原因是 const int* number2 声明了一个指向 const int的指针。
+    原因是 const int* numberPtr 声明了一个指向 const int 的指针。
     这意味着通过该指针不能修改它所指向的值，即指针指向的内容是只读的。
-    *number2 = 10; 试图通过指针修改所指向的值，这违反了 const 的约束，因此会导致编译错误。
     */
-    // *number2 = 10;  // error, 不能赋值
+    // *numberPtr = 10;  // error, 试图通过指针修改所指向的值，这违反了 const 的约束
+
+    // 输出 numberPtr: 0x61fddc *numberPtr: 10
+    cout << "numberPtr: " << numberPtr << " *numberPtr: " << *numberPtr << endl;
+
 }
 
-int main(int argc, char const* argv[]) { return 0; }
+int main(int argc, char const* argv[]) {
+    test1();
+    return 0;
+}
--- const/PointerConst.cpp
+++ const/PointerConst.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main(int argc, char const* argv[]) {
     int value = 10;
+    const int constValue = 20;
     int* const ptr = &value;  // 指针常量，指向 value 的地址
 
-    // 指向常量的常量指针，即不能修改指针指向的内容，也不能修改指针指向的地址
+    // 假如可以赋值，那么就可以通过 ptr 来修改内容，和 const 约束冲突了。
+    // int* const ptr = &constValue;  // error, constValue 是常量，不能修改指向的地址
+    // 解决方法：同时为指针添加 const 关键字，不能通过指针修改内容。
+    // const int* const ptr = &constValue;
+
+    // 指向常量的常量指针，即不能修改指针指向的内容，也不能修改指针地址
     const int* const ptr2 = &value;
     // *ptr2 = 20; // error
     // ptr2 = nullptr;  // error
