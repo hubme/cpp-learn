b09d3c27adab5c67961486cd9ea73740af09cc67
Vance <huogxu@gmail.com>
2025-05-14 10:25:28 +0800

    指针和引用

--- Reference.cpp
+++ Reference.cpp
@@ -8,7 +8,13 @@ void incement(int* number) {
 
 void incement2(int& number) { number++; }
 
-int main() {
+class Test {
+   public:
+    void test1();
+    void test2();
+};
+
+void Test::test1() {
     int number = 5;
     int* ptr = &number;
     // number 的引用。相当于创建了一个 number 变量的别名
@@ -37,6 +43,26 @@ int main() {
     cout << " ref=" << *ref;
     *ref = 21;
     cout << " ref=" << *ref << endl;
+}
+
+void Test::test2() {
+    int a = 1;
+    int b = 2;
+
+    int* p = &a;  // 初始化指针p指向变量a
+    int& r = a;   // 初始化引用r指向变量a
+    cout << "a = " << a << ", b = " << b << ", *p = " << *p << ", r = " << r << endl;
+
+    p = &b;  // 指针p指向了变量b
+    r = b;   // 引用r依然指向a，但a的值变成了b，即将 b 的值赋值给了 a
+
+    cout << "a = " << a << ", b = " << b << ", *p = " << *p << ", r = " << r << endl;
+}
+
+int main() {
+    Test test;
+    // test.test1();
+    test.test2();
 
     return 0;
 }
