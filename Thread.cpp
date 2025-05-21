fd680ce0e283e2e3ead67a7f1e83d1bca235e15c
Vance <huogxu@gmail.com>
2025-05-21 16:42:07 +0800

    Thread

--- Thread.cpp
+++ Thread.cpp
@@ -1,38 +1,48 @@
+#include <chrono>
 #include <iostream>
 #include <mutex>
 #include <thread>
 
+using namespace std;
+
 std::mutex myMutex;
 
 void printMessage(int count) {
+    this_thread::sleep_for(chrono::seconds(3));
+    cout << "threadId: " << this_thread::get_id() << endl;
     for (int i = 0; i < count; ++i) {
-        std::cout << "printMessage " + std::to_string(i) << std::endl;
+        cout << "printMessage " + to_string(i) << endl;
     }
 }
 
 class PrintTask {
    public:
     void operator()(int count) const {
+        cout << "threadId: " << this_thread::get_id() << endl;
         for (int i = 0; i < count; ++i) {
-            std::cout << "PrintTask " + std::to_string(i) << std::endl;
+            cout << "PrintTask " + to_string(i) << endl;
         }
     }
 };
 
 void testNewThread() {
+    cout << "threadId main: " << this_thread::get_id() << endl;
+
     // 1. 使用函数指针
-    std::thread myThread1(printMessage, 2);
-    myThread1.join();
+    thread myThread1(printMessage, 2);
+    if (myThread1.joinable()) {
+        myThread1.join();
+    }
 
     // 2. 使用函数对象
-    std::thread myThread2(PrintTask(), 2);
+    thread myThread2(PrintTask(), 2);
     myThread2.join();
 
     // 3. 使用 lambda 表达式
-    std::thread myThread3(
+    thread myThread3(
         [](int count) {
             for (int i = 0; i < count; ++i) {
-                std::cout << "lambda " + std::to_string(i) << std::endl;
+                cout << "lambda " + to_string(i) << endl;
             }
         },
         2);
@@ -45,19 +55,19 @@ void safeFunction() {
     // 请求锁定互斥量
     myMutex.lock();
     // 访问或修改共享资源
-    std::cout << "哈哈哈" << std::endl;
+    cout << "哈哈哈" << endl;
     // 释放互斥量
     myMutex.unlock();
 }
 
 void testSyncThread() {
     for (size_t i = 0; i < 100; i++) {
-        std::thread myThread(safeFunction);
+        thread myThread(safeFunction);
         myThread.join();
     }
 }
 
 int main(int argc, char const *argv[]) {
-    testSyncThread();
+    testNewThread();
     return 0;
 }
